d98be8f83d29492eec1c92fb19e09bedfa7ebf5d
OlesyaU <1600174@mail.ru>
2022-03-10 17:07:57 +0300

    Initial Commit

---
+++ test IB Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test IB Instruments
+//
+//  Created by Олеся on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
