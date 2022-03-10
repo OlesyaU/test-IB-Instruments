e52d0675e74f9ad7483d1228dba72f8f4a78d3b6
OlesyaU <1600174@mail.ru>
2022-03-10 17:26:36 +0300

    first commit from test IB Instruments for testHome (COMMIT) don't know where it will be com

--- test IB Instruments/ViewController.swift
+++ test IB Instruments/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   let nude = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
